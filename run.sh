503df0a830f3611b37b175a57bce46ed97cde7c2
DioRoman <roman-ronin@yandex.ru>
2025-07-30 12:12:00 +0300

    0.5.1

--- run.sh
+++ run.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-export DB_HOST=$(terraform output -raw mysql_host)
-export DB_NAME=$(terraform output -raw mysql_database)
-export DB_USER=$(terraform output -raw mysql_user)
-export DB_PASSWORD=$(terraform output -raw mysql_password)
-git clone https://github.com/DioRoman/shvirtd-example-python.git
-mv shvirtd-example-python /opt
-docker compose -f /opt/shvirtd-example-python/compose.yaml up -d
+git clone https://github.com/DioRoman/shvirtd-example-python-final.git
+mv shvirtd-example-python-final /opt
+docker compose -f /opt/shvirtd-example-python-final/compose.yaml up -d
